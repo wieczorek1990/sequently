fd8b255e9a6e3f94cb1b7db81257ab1e96e86d8e
Łukasz Wieczorek <wieczorek1990@gmail.com>
2019-08-28 13:34:54 +0200

    --allow-empty

--- dist/sequently.js
+++ dist/sequently.js
@@ -95,37 +95,37 @@ __webpack_require__.r(__webpack_exports__);
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   sequence: function (functions, args) {
-    let function_args;
-    let function_primes = [];
+    let functionArgs;
+    let functionPrimes = [];
     let incrementers = [];
-    let prime_index = 0;
+    let primeIndex = 0;
 
     if (!Array.isArray(args)) {
-      function_args = Array(functions.length).fill([]);
+      functionArgs = Array(functions.length).fill([]);
     } else {
-      function_args = args;
+      functionArgs = args;
     }
 
     for (let index = 0; index < functions.length; index += 1) {
       let incrementer = function () {
-        prime_index += 1;
-        function_primes[prime_index](incrementers[prime_index]);
+        primeIndex += 1;
+        functionPrimes[primeIndex](incrementers[primeIndex]);
       };
 
       incrementers.push(incrementer);
 
-      let function_prime = function (callback) {
-        if (function_args[prime_index].length === 0) {
-          functions[prime_index](callback);
+      let functionPrime = function (callback) {
+        if (functionArgs[primeIndex].length === 0) {
+          functions[primeIndex](callback);
         } else {
-          functions[prime_index](function_args[prime_index], callback);
+          functions[primeIndex](functionArgs[primeIndex], callback);
         }
       };
 
-      function_primes.push(function_prime);
+      functionPrimes.push(functionPrime);
     }
 
-    function_primes[0](incrementers[0], function_args[0]);
+    functionPrimes[0](incrementers[0], functionArgs[0]);
   }
 });
 
